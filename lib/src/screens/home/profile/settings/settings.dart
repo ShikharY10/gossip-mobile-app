6ea288b07fecd0bb0b52a9f18fa5e03fdcaab52b
Shikhar Yadav <yshikharfzd10@gmail.com>
2023-04-03 02:09:27 +0530

    feature: started development of settings page

---
+++ lib/src/screens/home/profile/settings/settings.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Setting extends StatefulWidget {
+  const Setting({Key? key}) : super(key: key);
+
+  @override
+  State<Setting> createState() => _SettingState();
+}
+
+class _SettingState extends State<Setting> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
